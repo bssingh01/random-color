cdc3a68f31bf58e81c5f61863e8d7bf6c226cb13
Bajrang Sharan Singh <86580690+bssingh01@users.noreply.github.com>
2024-02-09 15:48:52 +0530

    Update colorGenerator.js

--- colorGenerator.js
+++ colorGenerator.js
@@ -1,26 +1,22 @@
-let btn = document.querySelector('button');
-let box = document.querySelector('.box');
-let heading = document.querySelector('h1');
+let btn = document.querySelector("button");
+let box = document.querySelector(".box");
 
-let colorCode = document.createElement('p');
+let colorCode = document.createElement("h2");
 colorCode.innerHTML = "rgb(255, 255, 255)";
-box.insertAdjacentElement('beforeend', colorCode);
+box.insertAdjacentElement("beforeend", colorCode);
 
-function colorGenerate () {
-    let r = Math.floor(Math.random()*256);
-    let g = Math.floor(Math.random()*256);
-    let b = Math.floor(Math.random()*256);
+function colorGenerate() {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
 
-    randColor = `rgb(${r},${g}, ${b})`;
-    return randColor;
+  randColor = `rgb(${r},${g}, ${b})`;
+  return randColor;
 }
 
-btn.addEventListener("click", function(){
-    let color = colorGenerate();
-
-    this.style.backgroundColor = color;
-    box.style.backgroundColor = color;
-    heading.style.color = color;
-    console.log(`color is set to ${color}`);
-    colorCode.innerHTML = color;
+btn.addEventListener("click", function () {
+  let color = colorGenerate();
+  document.body.style.backgroundColor = color;
+  console.log(`color is set to ${color}`);
+  colorCode.innerHTML = color;
 });
